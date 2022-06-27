9ff98f8673b3ec8fcbaf8ebfb32541800b7d69df
endritNovaku <ennald@outlook.com>
2022-06-27 04:27:45 -0700

    create user class

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+"""
+Defines a User model
+"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """
+    User class
+    """
+
+    email = ""
+    password: = ""
+    first_name = ""
+    last_name = ""
