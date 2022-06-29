5c9bd4893fd9ce0fa2dacea4659efdb8450ae5b2
KristiSeraj <serajkristi@gmail.com>
2022-06-29 00:56:14 -0700

    console added

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Console program
+"""
+from models.base_model import BaseModel
+from models.city import City
+from models.state import State
+from models.user import User
+from models.review import Review
+from models.amenity import Amenity
+from models.place import Place
+import cmd
+from models import storage
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class for the interpreter
+    """
+    cmd_prompt = '(hbnb)'
+    cmd_classes = {}
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
