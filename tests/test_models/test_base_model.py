69cb96bce34f73f9fd51883cb4c0f714e491e76c
endritNovaku <ennald@outlook.com>
2022-07-01 08:44:03 -0700

    unittesing for base_model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python3
+"""Test module"""
+import unittest
+import models
+from time import sleep
+from models.base_model import BaseModel
+from datetime import datetime
+from uuid import UUID
+import cmd
+from models import storage
+import os
+
+class TestBaseModel(unittest.TestCase):
+    """Unit test for BaseModel"""
+
+    def test_no_args(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(BaseModel().id))
+
+    def test_stored_in_objects(self):
+        self.assertIn(BaseModel(), models.storage.all().values())
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+
+    def test_two_models_different_created_at(self):
+        base1 = BaseModel()
+        sleep(0.05)
+        base2 = BaseModel()
+        self.assertLess(base1.created_at, base2.created_at)
+
+    def test_two_models_different_updated_at(self):
+        base1 = BaseModel()
+        sleep(0.05)
+        base2 = BaseModel()
+        self.assertLess(base1.updated_at, base2.updated_at)
+
+    def test_two_models_unique_ids(self):
+        base1 = BaseModel()
+        base2 = BaseModel()
+        self.assertNotEqual(base1.id, base2.id)
+
+    def test_args_unused(self):
+        base = BaseModel(None)
+        self.assertNotIn(None, base.__dict__.values())
+
+    def test_instantiation_with_kwargs(self):
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        bm = BaseModel(id="345", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(bm.id, "345")
+        self.assertEqual(bm.created_at, dt)
+        self.assertEqual(bm.updated_at, dt)
+
+    def test_str_representation(self):
+        dt = datetime.today()
+        dt_repr = repr(dt)
+        bm = BaseModel()
+        bm.id = "123456"
+        bm.created_at = bm.updated_at = dt
+        bmstr = bm.__str__()
+        self.assertIn("[BaseModel] (123456)", bmstr)
+        self.assertIn("'id': '123456'", bmstr)
+        self.assertIn("'created_at': " + dt_repr, bmstr)
+        self.assertIn("'updated_at': " + dt_repr, bmstr)
+
+    def test_instantiation_with_None_kwargs(self):
+        with self.assertRaises(TypeError):
+            BaseModel(id=None, created_at=None, updated_at=None)
+
+    def test_instantiation_with_args_and_kwargs(self):
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        bm = BaseModel("12", id="345", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(bm.id, "345")
+        self.assertEqual(bm.created_at, dt)
+        self.assertEqual(bm.updated_at, dt)
+
+    """Unit tests for testing save method"""
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def test_one_save(self):
+        base = BaseModel()
+        sleep(0.05)
+        first_updated_at = bm.updated_at
+        base.save()
+        self.assertLess(first_updated_at, base.updated_at)
+
+    def test_two_saves(self):
+        base = BaseModel()
+        sleep(0.05)
+        first_updated_at = base.updated_at
+        base.save()
+        second_updated_at = base.updated_at
+        self.assertLess(first_updated_at, second_updated_at)
+        sleep(0.05)
+        base.save()
+        self.assertLess(second_updated_at, base.updated_at)
+
+    def test_save_with_arg(self):
+        base = BaseModel()
+        with self.assertRaises(TypeError):
+            bm.save(None)
+
+    def test_save_updates_file(self):
+        base = BaseModel()
+        base.save()
+        bmid = "BaseModel." + base.id
+        with open("file.json", "r") as f:
+            self.assertIn(bmid, f.read())
+
+if __name__ == "__main__":
+    unittest.main()
