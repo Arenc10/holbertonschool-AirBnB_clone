a41ce803a653b69242bf64d9c599f8c21536978c
KristiSeraj <serajkristi@gmail.com>
2022-07-01 01:00:58 -0700

    test added

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Test module"""
+import unittest
+from models.base_model import BaseModel
+from models.city import City
+from models.state import State
+from models.user import User
+from models.review import Review
+from models.amenity import Amenity
+from models.place import Place
+import cmd
+from models import storage
+
