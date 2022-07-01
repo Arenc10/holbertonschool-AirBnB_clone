25801210549578e1e347203e056250ee881e149f
KristiSeraj <serajkristi@gmail.com>
2022-07-01 05:23:11 -0700

    __init__ files added, filestorage test created

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Test module"""
+import json
+import os
+import models
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import
+
+
+class TestFileStorage(unittest.TestCase):
+
